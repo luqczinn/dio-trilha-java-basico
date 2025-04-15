bfca040442143d36cae0a322b9fb759ddce90be1
luqczinn <lucasfofs7@gmail.com>
2025-04-15 11:44:05 -0300

    Criação do projeto ContaBanco, no qual coleta informações da conta bancária do usuário através do console e as exibe em uma String concatenada.

--- ContaBanco/src/edu/lucas/contabanco/ContaTerminal.java
+++ ContaBanco/src/edu/lucas/contabanco/ContaTerminal.java
@@ -1,10 +1,31 @@
 package edu.lucas.contabanco;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("teste");
+		// TODO 
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o número da Agência Bancária: ");
+		
+		String agencia = scan.nextLine();
+		
+		System.out.println("Digite o número da Conta Bancária: ");
+		
+		int conta = Integer.parseInt(scan.nextLine());
+		
+		System.out.println("Digite o seu nome: ");
+		
+		String nome = scan.nextLine();
+		
+		System.out.println("Digite o saldo de sua conta: ");
+		
+		double saldo = Double.parseDouble(scan.nextLine());
+		
+		String res = "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + (", conta ") + conta + " e seu saldo " + saldo + " já está disponível para saque.";
+		System.out.println(res);
 	}
 
 }
