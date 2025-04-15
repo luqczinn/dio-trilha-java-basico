f858b0645eda5bf3a673571093d1e016ba6779f9
luqczinn <lucasfofs7@gmail.com>
2025-04-15 10:49:52 -0300

    Commit inicial

---
+++ ContaBanco/src/edu/lucas/contabanco/ContaTerminal.java
@@ -0,0 +1,10 @@
+package edu.lucas.contabanco;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("teste");
+	}
+
+}
